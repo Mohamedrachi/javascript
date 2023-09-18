25081ca20c8015aa22bb2663a7acfb65c54864b2
Mohamed Ali Hussein <jaamkaal80@gmail.com>
2023-09-18 23:47:02 +0800

    1-5

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("hello world")
+console.log("mohamed ali")
+let ali = "legend" //(let) markasta oo aan isticmaalno let javascri waxan ushegayaa inaan samynyno variable.
+console.log(ali)
+ali = "iconic"
+console.log(ali)
+const ahmed = "moalimow"//(const) waxay la mid tahay led lkn waxay ku kala duwan yihiin ma badali kartid value.
+console.log(ahmed)
+// DATA TYPES
+let name = "Firkcamp is a coding bootcamp" //string
+let age = 30 //number
+let hungry = false //boolean
+
+console.log(typeof hungry)
+console.log(typeof age)
+console.log(typeof name)
+
+
