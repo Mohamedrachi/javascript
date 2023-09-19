4dc7c4977507ad268d15a9236b57f8e87f933853
Mohamed Ali Hussein <jaamkaal80@gmail.com>
2023-09-20 00:28:13 +0800

    6-13

--- index.js
+++ index.js
@@ -7,12 +7,68 @@ console.log(ali)
 const ahmed = "moalimow"//(const) waxay la mid tahay led lkn waxay ku kala duwan yihiin ma badali kartid value.
 console.log(ahmed)
 // DATA TYPES
-let name = "Firkcamp is a coding bootcamp" //string
-let age = 30 //number
+let nAme = "Firkcamp is a coding bootcamp" //string
+let Age = 30 //number
 let hungry = false //boolean
 
 console.log(typeof hungry)
-console.log(typeof age)
-console.log(typeof name)
+console.log(typeof Age)
+console.log(typeof nAme)
+
+
+let name = "Fikrcamp"
+let cohort = "Cohort 3"
+
+// I am a cohort 3 student at fikrcamp
+
+console.log("I am a " + cohort + " student at " + name)
+
+console.log(`I am a ${cohort} student at ${name}`)
+//ARITHMETIC OPERATION
+let x = 10
+let y = 7
+console.log(x + y)
+console.log(x - y)
+console.log(x/y)
+console.log(40+5)
+//operators
+let m = 4
+let l = 5
+console.log(m == l)
+console.log(m === l)
+console.log(m != l)
+//if statement
+const age = 26
+if(age < 50){
+    console.log("i am still a young ")
+}
+//elseif
+let Name = "abdi"
+
+if(Name === "ali"){
+    console.log("Hi Ali")
+}else if(Name === "mo"){
+    console.log("Hi Mo")
+}else if(Name === "abdi"){
+    console.log("Hi Abdi")   
+}else{
+    console.log("Yout not Ali or Mo")
+}
+//else
+let AGE = 10
+
+if(AGE>18){
+    console.log("Your an adult")
+}else{
+    console.log("Your under 18")
+}
+//exercise
+let k = 7
+let g = 6
+if (k > g){
+    console.log(`${k} is greater then ${g}`)
+}
+
+
 
 
