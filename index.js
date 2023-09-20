c17841d6b1f4477bba5d937014e50d583f812d18
Mohamed Ali Hussein <jaamkaal80@gmail.com>
2023-09-20 20:56:02 +0800

    JS-1-done

--- index.js
+++ index.js
@@ -62,13 +62,87 @@ if(AGE>18){
 }else{
     console.log("Your under 18")
 }
-//exercise
+//exercise 1
 let k = 7
 let g = 6
 if (k > g){
     console.log(`${k} is greater then ${g}`)
 }
-
-
-
-
+//logicaloperatorexample
+let hascar = true
+let haslicense = false
+let agE = 21
+if(agE > 18 && haslicense === false && hascar === true){
+    console.log("you can drive")
+}else{
+    console.log("go walk!")
+}
+//exercise 2
+let grade = 1-100
+if(100 - 90){
+    console.log("You Get A Grade!")
+}
+if(90 - 75){
+    console.log("You Get B Grade")
+}
+if(75 - 60){
+    console.log("You Get C Grade")
+}
+if(60){
+    console.log("You Get an Failed")
+}
+//javascript part one Assignment
+//Variables and Values exercise
+let country = "somalia"
+let continent = "Africa"
+let population = 50 
+console.log(`${country} ${continent} ${population}`)
+let language = 100
+let onCoust = true
+//Basic operators exercise
+population = 25
+population++
+console.log(`${population}`)
+console.log(true)
+let francepopulation = 33
+let mycountrypopulation =25
+console.log(francepopulation > mycountrypopulation)
+let description = "YOURCOUNTRY is in YOURCONTINENET and its YOURPOPULATION million people speak YOURLANGUAGE"
+//If & Else statements
+let cp = 35
+if(cp > 33){
+   console.log("YOURCOUNTRY's population is above average")
+}else{
+    console.log("YOURCOUNTRY's population is YOURPOPULATION, bellow average'")
+}
+//Logical Operators
+let copo = 40
+let coast = false
+if(copo < 50 && coast == false){
+    console.log("you should live in YOURCOUNTRY!")
+}else{
+    console.log("YOURCOUNTRY does not meet your criteria")
+}
+//If & else Statements
+let chineseormandarin = 1
+let spanish = 2
+let english = 3
+let hindi = 4
+let arabic = 5
+if(chineseormandarin = 1){
+    console.log("MOST number of native speakers")
+}
+if(spanish = 2){
+    console.log("2nd place in number of native speakers")
+}
+if(english = 3){
+    console.log("3rd place")
+}
+if(hindi = 4){
+    console.log("Number 4")
+}
+if(arabic = 5){
+    console.log("5th most spoken language")
+}else{
+    console.log("Great languages too ")
+}
