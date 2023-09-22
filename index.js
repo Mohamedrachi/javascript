1b32039a5910214fc231cecb2e803a538cf3bcbe
Mohamed Ali Hussein <jaamkaal80@gmail.com>
2023-09-22 10:09:03 +0800

    comp+new

--- index.js
+++ index.js
@@ -1,136 +1,136 @@
-console.log("hello world")
-console.log("mohamed ali")
-let ali = "legend" //(let) markasta oo aan isticmaalno let javascri waxan ushegayaa inaan samynyno variable.
-console.log(ali)
-ali = "iconic"
-console.log(ali)
-const ahmed = "moalimow"//(const) waxay la mid tahay led lkn waxay ku kala duwan yihiin ma badali kartid value.
-console.log(ahmed)
-// DATA TYPES
-let nAme = "Firkcamp is a coding bootcamp" //string
-let Age = 30 //number
-let hungry = false //boolean
+// console.log("hello world")
+// console.log("mohamed ali")
+// let ali = "legend" //(let) markasta oo aan isticmaalno let javascri waxan ushegayaa inaan samynyno variable.
+// console.log(ali)
+// ali = "iconic"
+// console.log(ali)
+// const ahmed = "moalimow"//(const) waxay la mid tahay led lkn waxay ku kala duwan yihiin ma badali kartid value.
+// console.log(ahmed)
+// // DATA TYPES
+// let nAme = "Firkcamp is a coding bootcamp" //string
+// let Age = 30 //number
+// let hungry = false //boolean
 
-console.log(typeof hungry)
-console.log(typeof Age)
-console.log(typeof nAme)
+// console.log(typeof hungry)
+// console.log(typeof Age)
+// console.log(typeof nAme)
 
 
-let name = "Fikrcamp"
-let cohort = "Cohort 3"
+// let name = "Fikrcamp"
+// let cohort = "Cohort 3"
 
-// I am a cohort 3 student at fikrcamp
+// // I am a cohort 3 student at fikrcamp
 
-console.log("I am a " + cohort + " student at " + name)
+// console.log("I am a " + cohort + " student at " + name)
 
-console.log(`I am a ${cohort} student at ${name}`)
-//ARITHMETIC OPERATION
-let x = 10
-let y = 7
-console.log(x + y)
-console.log(x - y)
-console.log(x/y)
-console.log(40+5)
-//operators
-let m = 4
-let l = 5
-console.log(m == l)
-console.log(m === l)
-console.log(m != l)
-//if statement
-const age = 26
-if(age < 50){
-    console.log("i am still a young ")
-}
-//elseif
-let Name = "abdi"
+// console.log(`I am a ${cohort} student at ${name}`)
+// //ARITHMETIC OPERATION
+// let x = 10
+// let y = 7
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x/y)
+// console.log(40+5)
+// //operators
+// let m = 4
+// let l = 5
+// console.log(m == l)
+// console.log(m === l)
+// console.log(m != l)
+// //if statement
+// const age = 26
+// if(age < 50){
+//     console.log("i am still a young ")
+// }
+// //elseif
+// let Name = "abdi"
 
-if(Name === "ali"){
-    console.log("Hi Ali")
-}else if(Name === "mo"){
-    console.log("Hi Mo")
-}else if(Name === "abdi"){
-    console.log("Hi Abdi")   
-}else{
-    console.log("Yout not Ali or Mo")
-}
-//else
-let AGE = 10
+// if(Name === "ali"){
+//     console.log("Hi Ali")
+// }else if(Name === "mo"){
+//     console.log("Hi Mo")
+// }else if(Name === "abdi"){
+//     console.log("Hi Abdi")   
+// }else{
+//     console.log("Yout not Ali or Mo")
+// }
+// //else
+// let AGE = 10
 
-if(AGE>18){
-    console.log("Your an adult")
-}else{
-    console.log("Your under 18")
-}
-//exercise 1
-let k = 7
-let g = 6
-if (k > g){
-    console.log(`${k} is greater then ${g}`)
-}
-//logicaloperatorexample
-let hascar = true
-let haslicense = false
-let agE = 21
-if(agE > 18 && haslicense === false && hascar === true){
-    console.log("you can drive")
-}else{
-    console.log("go walk!")
-}
-//exercise 2
-let grade = 65
-if(grade > 90 && grade < 100){
-    console.log("You Get A Grade!")
-}
-else if(grade > 75 && grade < 90){
-    console.log("You Get B Grade")
-}
-else if(grade > 60 && grade < 75){
-    console.log("You Get C Grade")
-}
-else if(grade < 60){
-    console.log("You FAILED!")
-}
-//javascript part one Assignment
-//Variables and Values exercise
-let country = "somalia"
-let continent = "Africa"
-let population = 500000000 
-console.log(`${country} ${continent} ${population}`)
-let language = "English"
-let onCoust = true
-//Basic operators exercise
-console.log(population / 2)
-console.log(population++)
-console.log(110000000 < population)
-console.log(`${country} is in ${continent} and it is ${population} million people speak${language} `)
-//If & Else statements
-if(population > 330000000){
-   console.log(`${country} population is above average`)
-}else{
-    console.log(`${country} population is ${population}, bellow average`)
-}
-//Logical Operators
-if(population > 500000000 && onCoust === false){
-    console.log(`you should live in ${country}!`)
-}else{
-    console.log(`${country} does not meet your criteria`)
-}
-//If & else Statements
-if(language === "chinese" || language === "madrints"){
+// if(AGE>18){
+//     console.log("Your an adult")
+// }else{
+//     console.log("Your under 18")
+// }
+// //exercise 1
+// let k = 7
+// let g = 6
+// if (k > g){
+//     console.log(`${k} is greater then ${g}`)
+// }
+// //logicaloperatorexample
+// let hascar = true
+// let haslicense = false
+// let agE = 21
+// if(agE > 18 && haslicense === false && hascar === true){
+//     console.log("you can drive")
+// }else{
+//     console.log("go walk!")
+// }
+// //exercise 2
+// let grade = 65
+// if(grade > 90 && grade < 100){
+//     console.log("You Get A Grade!")
+// }
+// else if(grade > 75 && grade < 90){
+//     console.log("You Get B Grade")
+// }
+// else if(grade > 60 && grade < 75){
+//     console.log("You Get C Grade")
+// }
+// else if(grade < 60){
+//     console.log("You FAILED!")
+// }
+// //javascript part one Assignment
+// //Variables and Values exercise
+// let country = "somalia"
+// let continent = "Africa"
+// let population = 500000000 
+// console.log(`${country} ${continent} ${population}`)
+// let language = "English"
+// let onCoust = true
+// //Basic operators exercise
+// console.log(population / 2)
+// console.log(population++)
+// console.log(110000000 < population)
+// console.log(`${country} is in ${continent} and it is ${population} million people speak${language} `)
+// //If & Else statements
+// if(population > 330000000){
+//    console.log(`${country} population is above average`)
+// }else{
+//     console.log(`${country} population is ${population}, bellow average`)
+// }
+// //Logical Operators
+// if(population > 500000000 && onCoust === false){
+//     console.log(`you should live in ${country}!`)
+// }else{
+//     console.log(`${country} does not meet your criteria`)
+// }
+// //If & else Statements
+// if(language === "chinese" || language === "madrints"){
 
-    console.log("MOST number of native speakers")
-}else if(language === "spanish"){
-    console.log("2nd")
-}
-else if(language === "english"){
-    console.log("3rd")
-}else if(language === "hindi"){
-    console.log("4th")}
-else if(language === "arabic"){
-    console.log("5th")
-}else{
-    console.log("Great Languages Too")
-} 
+//     console.log("MOST number of native speakers")
+// }else if(language === "spanish"){
+//     console.log("2nd")
+// }
+// else if(language === "english"){
+//     console.log("3rd")
+// }else if(language === "hindi"){
+//     console.log("4th")}
+// else if(language === "arabic"){
+//     console.log("5th")
+// }else{
+//     console.log("Great Languages Too")
+// } 
 
 
