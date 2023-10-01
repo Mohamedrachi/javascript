00053f5dd0e1b13a7e8d9f545d32ec04aa79ccee
Mohamed Ali Hussein <jaamkaal80@gmail.com>
2023-10-02 00:25:16 +0800

    4-6

--- manipulation.js
+++ manipulation.js
@@ -10,5 +10,28 @@ document.querySelector(".hallow").style.color = "blue"
 // })
 document.querySelector("#sumbit").addEventListener("click",function(){
     document.getElementById("heading").style.color = "yellow"
-    document.querySelector("#container").style.background = "black"
+    document.querySelector("#ali").style.background = "black"
+})
+
+// can i drive (small application)
+document.querySelector("#check").addEventListener("click",function(){
+    //1. the value from the textbox
+   let age =  document.querySelector("#input").value
+    //2. condition age >= 18  =>  background green
+   //   age < 18  =>   background red
+   if( age >= 18){
+    document.getElementById("container").style.background = "green"
+    }else {
+        document.getElementById("container").style.background = "red"
+   }
+
+
+})
+// BMI APP
+document.querySelector("#b2").addEventListener("click",function(){
+    let height =  document.querySelector("#in1").value
+    let mass =  document.querySelector("#in2").value
+    let BMI =  mass / (height * height)
+    
+    document.querySelector("#bmi").innerText = `Your BMI is ${BMI}`
 })
